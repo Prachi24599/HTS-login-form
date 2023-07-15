5347b9b8213c133b60740bad3009108239180b65
Prachi24599 <prachi24599@gmail.com>
2023-07-15 11:05:51 +0530

    backgroud style added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Login from "./components/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <div className="App">
+      <Login />;
+    </div>
+  );
 }
 
 export default App;
