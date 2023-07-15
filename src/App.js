2382424b51eecfa603da4a4dfdd8366041e93f00
Prachi24599 <prachi24599@gmail.com>
2023-07-15 23:11:27 +0530

    redirect to profile on submit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-      <Login />;
+      {/* <Login /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import "./Login.css";
+import Profile from "./Profile";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
   const [error, setError] = useState(false);
   const [validEmailMsg, setValidEMailMsg] = useState("");
+  let submitform = true;
 
   function changeHandler(event) {
     setFormData((prevFormData) => ({
@@ -18,21 +23,26 @@ const Login = () => {
 
   function submitHandler(event) {
     event.preventDefault();
-
     //checking if the field is empty
     if (formData.email.length === 0 || formData.password.length === 0) {
       setError(true);
+      submitform = false;
     }
-
     //email validation
     const regEx =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
     if (!regEx.test(formData.email) && formData.email.length > 0) {
       setValidEMailMsg("please enter valid email address");
+      submitform = false;
+    } else {
+      setValidEMailMsg("");
     }
 
-    //print form data on console
+    if (submitform) {
+      //print form data on console
+      console.log(formData);
+      navigate("/profile");
+    }
   }
 
   return (
@@ -57,7 +67,6 @@ const Login = () => {
           ""
         )}
         <p className="errorMsg">{validEmailMsg}</p>
-
         <label htmlFor="password">
           <p className="label">
             Password <sup>*</sup>
