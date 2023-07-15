92f740d5b0d53133fcf2c5413960ada76c62a049
Prachi24599 <prachi24599@gmail.com>
2023-07-15 10:57:09 +0530

    login form added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function changeHandler(event) {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="email">
+        <p className="label c">
+          Email <sup>*</sup>
+        </p>
+        <input
+          required
+          type="email"
+          value={formData.email}
+          name="email"
+          placeholder="Enter email address"
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        <p className="label">
+          Password <sup>*</sup>
+        </p>
+        <input
+          required
+          type="password"
+          value={formData.password}
+          name="password"
+          placeholder="Enter password"
+          onChange={changeHandler}
+        />
+      </label>
+      <br />
+      <button>Sign in</button>
+    </form>
+  );
 };
 
 export default Login;
